386fe3268427a93d55bbf33c00d3ab8ca3c5fc8f
deimos31 <luis.itzhak13@gmail.com>
2021-11-11 22:34:14 -0600

    what i made in day 1

---
+++ dia1/e1.py
@@ -0,0 +1,27 @@
+#int
+a=2
+#float
+b=2.3
+#string cadena de caracteres
+c = "hello world"
+#boolean
+d=True
+e=False
+
+print(c)
+
+x=1
+x=x+1
+print(x)
+
+#type data
+city=str(input("type your city where you grew up:"))
+print(type(city))
+
+num1=int(input("type your first number: "))
+num2=int(input("type a second number:"))
+sum= num1+ num2 
+print (sum)
+print(f"Sum= {sum}")
+
+
---
+++ dia1/e2.py
@@ -0,0 +1,5 @@
+print("Welcome to the band name generator")
+city= input("whats the name of the city where you grew up?:")
+pet= input("whats the name of your pet?:")
+print(f"Your band name could be: {city} {pet}")
+
